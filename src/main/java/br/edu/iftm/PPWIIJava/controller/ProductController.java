a0c33b84981025c72d752af8d44172d0cfc69442
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2025-03-12 15:20:41 -0300

    adicionando funcionalidade de listar todos produtos

--- src/main/java/br/edu/iftm/PPWIIJava/controller/ProductController.java
+++ src/main/java/br/edu/iftm/PPWIIJava/controller/ProductController.java
@@ -1,15 +1,28 @@
 package br.edu.iftm.PPWIIJava.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.edu.iftm.PPWIIJava.model.Product;
+import br.edu.iftm.PPWIIJava.service.ProductService;
 
 
 @Controller
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/product")
-    public String getMethodName() {
-        return "/product/index";
+    public String index(Model model) {
+        model.addAttribute("productsList", productService.getAllProducts());
+        return "product/index";
     }
-    
+  
 }
