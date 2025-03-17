f199d0a0ed2d317b53625c932593109583926290
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2025-03-17 15:02:46 -0300

    Validacao dos campos do produto no cadastro e exibicao de erros no form

--- src/main/java/br/edu/iftm/PPWIIJava/controller/ProductController.java
+++ src/main/java/br/edu/iftm/PPWIIJava/controller/ProductController.java
@@ -3,6 +3,7 @@ package br.edu.iftm.PPWIIJava.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import br.edu.iftm.PPWIIJava.model.Product;
 import br.edu.iftm.PPWIIJava.service.ProductService;
+import jakarta.validation.Valid;
 
 
 
@@ -32,7 +34,10 @@ public class ProductController {
     }
 
     @PostMapping("/product/save")
-    public String postMethodName(@ModelAttribute Product product) {
+    public String postMethodName(@ModelAttribute @Valid Product product, BindingResult result) {
+        if (result.hasErrors()) {
+            return "product/create";
+        }
         productService.saveProduct(product);
         return "redirect:/product";
     }
